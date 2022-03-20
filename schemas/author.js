68099a4ed790b128b3698166d17d95a560147acc
leejungho9 <9wjdgh9@naver.com>
2022-03-20 23:04:25 +0900

    create project

---
+++ schemas/author.js
@@ -0,0 +1,33 @@
+export default {
+  name: 'author',
+  title: 'Author',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+      validation : Rule => Rule.required()
+    },
+    {
+      name : 'role',
+      title : 'Role',
+      type : 'string',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      validation : Rule => Rule.required(),
+      options: {
+        hotspot: true,
+      },
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+}
